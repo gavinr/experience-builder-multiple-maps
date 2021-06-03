918d9fa27b17614b59fabc7359cf27a48b0754d3
Gavin Rehkemper <gavin@gavinr.com>
2021-06-02 20:19:28 -0500

    working

--- widgets/multiple-maps/src/runtime/widget.tsx
+++ widgets/multiple-maps/src/runtime/widget.tsx
@@ -1,19 +1,22 @@
 /** @jsx jsx */
 import {React, AllWidgetProps,  jsx } from 'jimu-core';
-import { IMConfig } from '../config';
 import { JimuMapViewComponent, JimuMapView } from "jimu-arcgis";
 
-export default class Widget extends React.PureComponent<AllWidgetProps<IMConfig>, any> {
+export default class Widget extends React.PureComponent<AllWidgetProps<any>, any> {
   constructor(props) {
     super(props);
 
+    // Initial state
     this.state = {
       jimuMapViews: [],
-      mapWidgetIds: []
+      mapWidgetIds: [],
+      selectValue: '',
+      lonLat: null
     };
   }
 
   componentDidMount() {
+    // When the widget loads, get all the mapViews from window._appState
     const mapWidgetIds = Object.values(window._appState.appConfig.widgets).filter((widgetInfo: any) => {
       return widgetInfo.uri === 'widgets/arcgis/arcgis-map/'
     }).map((widgetInfo: any) => {
@@ -24,9 +27,44 @@ export default class Widget extends React.PureComponent<AllWidgetProps<IMConfig>
     });
   }
 
+  /**
+   * Zooms to a point for a single jimuMapView
+   * 
+   * @param jmv JimuMapView
+   * @param lonLat [longitude, latitude]
+   */
+  jmvGoTo(jmv: JimuMapView, lonLat: number[]) {
+    jmv.view.goTo(lonLat);
+    jmv.view.zoom = 10;
+  }
+
+  /**
+   * Gets called when the select changes
+   * @param evt 
+   */
+  selectChangeHandler(evt) {
+    // Save the current value
+    this.setState({selectValue: evt.target.value});
+    
+    // Save the longitude/latitude so that if future maps get created, they
+    // will use this to zoom to.
+    const [longitude, latitude] = evt.target.value.split(',');
+    const lonLat = [parseFloat(longitude), parseFloat(latitude)];
+    this.setState({ lonLat });
+
+    // Loop through all existing JMVs and set the location.
+    this.state.jimuMapViews.forEach((jmv: JimuMapView) => {
+      this.jmvGoTo(jmv, lonLat);
+    });
+
+  }
+
   render() {
     return (
       <div className="widget-demo jimu-widget m-2">
+
+        {/* Loop through all mapWidgetIds and create a
+        JimuMapViewComponent for each */}
         {this.state.mapWidgetIds.map(widgetId => (
           <JimuMapViewComponent
             useMapWidgetId={widgetId}
@@ -35,22 +73,20 @@ export default class Widget extends React.PureComponent<AllWidgetProps<IMConfig>
               this.setState({
                 jimuMapViews: jimuMapViews,
               });
+              if (this.state.lonLat) {
+                this.jmvGoTo(jmv, this.state.lonLat);
+              }
             }}
           />
         ))}
 
         <div>
-          <select>
-            <option value="-87.629700, 41.873651">Chicago</option>
-            <option value="-90.196381, 38.622235">St. Louis</option>
-            <option value="-98.490028, 29.430029">San Antonio</option>
+          <select onChange={(e) => { this.selectChangeHandler(e); }} value={this.state.selectValue}>
+            <option value=""></option>
+            <option value="-87.629700,41.873651">Chicago</option>
+            <option value="-90.196381,38.622235">St. Louis</option>
+            <option value="-98.490028,29.430029">San Antonio</option>
           </select>
-        </div>
-
-        <div>
-          {this.state.jimuMapViews.map(jmv => (
-            <p>{jmv.datasourceId}</p>
-          ))}
         </div>
       </div>
     );
